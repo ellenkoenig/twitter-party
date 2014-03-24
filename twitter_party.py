fcb0e8ffe389f7606bbf12d56cbcdca66dad77d0
Ellen König <ellen@ellenkoenig.de>
2014-03-24 21:49:14 +0100

    fixing logging

--- twitter_party.py
+++ twitter_party.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session, url_for, redirect
 from flask_oauth import OAuth
 import os
 import logging
-from logging.handlers import StreamHandler
+from StreamHandler import StreamHandler
 
 app = Flask(__name__)
 
