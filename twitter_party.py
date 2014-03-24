2835448715f67d288d444b7783d80c768ed89fcd
Ellen König <ellen@ellenkoenig.de>
2014-03-24 21:24:44 +0100

    fixing login

--- twitter_party.py
+++ twitter_party.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session, url_for, redirect
 from flask_oauth import OAuth
 import os
 
